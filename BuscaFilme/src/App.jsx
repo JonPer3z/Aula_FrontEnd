2b518816654ae2d7f0b3025c5a7f99e83b1a5bf0
JonPer3z <perez.joaopedro@gmail.com>
2025-08-21 17:27:05 -0300

    Iniciando

--- BuscaFilme/src/App.jsx
+++ BuscaFilme/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { fetchMoviesBySearchTerm } from './api';
+import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  // O useEffect é um Hook que executa efeitos colaterais.
+  // Neste caso, ele faz a busca de filmes sempre que o 'searchTerm' muda
+  useEffect(() => {
+    // Função assíncrona para buscar os filmes
+    const getMovies = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const movieResults = await fetchMoviesBySearchTerm(searchTerm);
+        setMovies(movieResults);
+      } catch (err) {
+        setError("Não foi possível carregar os filmes. Verifique sua conexão e chave de API.")
+        setMovies([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+// A busca só é feita se houver um termo de busca
+if(searchTerm) {
+  // Usamos um pequeno atraso para evitar muitas requisições
+  const timer = setTimeout(() => {
+    getMovies();
+  }, 500); // 500 milissegundos
+
+  // Retornar uma função de limpeza para cancelar o times caso o termo de busca mude rapidamente.
+  return () => clearTimeout(timer);
+} else {
+  setMovies([]); // Limpa a lista se a busca estiver vazia
+}
+  }, [searchTerm]); // Array de dependências: a função é re-executada quando 'searchTerm' muda.
+
+  return(
+    <div className="container">
+      <h1>Buscador de Filmes</h1>
+      <input type="text" placeholder='Digite o nome de um filme...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="search-input"/>
+
+      {loading && <p>Carregando filmes...</p>}
+      {error && <p className="error">{error}</p>}
+    </div>
   )
 }
-
-export default App
