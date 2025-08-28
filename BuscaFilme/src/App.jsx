d57c0f69671f46cddb01b561989ae8360ab73600
JonPer3z <perez.joaopedro@gmail.com>
2025-08-28 17:14:23 -0300

    Finalização

--- BuscaFilme/src/App.jsx
+++ BuscaFilme/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fetchMoviesBySearchTerm } from "./Api";
+import "./App.css";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,8 +16,10 @@ function App() {
       try {
         const movieResults = await fetchMoviesBySearchTerm(searchTerm);
         setMovies(movieResults);
-      } catch (e) {
-        setError("Erro ao carregar os filmes:", e);
+      } catch (err) {
+        setError(
+          "Não foi possível carregar os filmes. Verifique sua conexão e chave de API.", err
+        );
         setMovies([]);
       } finally {
         setLoading(false);
@@ -26,36 +29,49 @@ function App() {
     if (searchTerm) {
       const timer = setTimeout(() => {
         getMovies();
-      }, 500);
+      }, 500); 
+
       return () => clearTimeout(timer);
     } else {
-      setMovies([]);
+      setMovies([]); 
     }
-  }, [searchTerm]);
+  }, [searchTerm]); 
 
   return (
     <div className="container">
       <h1>Buscador de Filmes</h1>
       <input
         type="text"
-        placeholder="Digite o nome de um filme:"
+        placeholder="Digite o nome de um filme..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         className="search-input"
       />
 
-      {loading && <p>Carregando filmes</p>}
+      {loading && <p>Carregando filmes...</p>}
       {error && <p className="error">{error}</p>}
 
-      {!loading && !error && movies.length === 0 && (
-        <p>Nenhum filme encontrado.</p>
+      {!loading && !error && movies.length === 0 && searchTerm !== "" && (
+        <p>Nenhum filme encontrado. Tente novamente.</p>
       )}
 
       {!loading && movies.length > 0 && (
-        <ul className="movie-list"></ul>
+        <ul className="movie-list">
+          {movies.map((movie) => (
+            <li key={movie.id} className="movie-item">
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className="movie-details">
+                <h3>{movie.title}</h3>
+                <p>Nota: {movie.vote_average}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
-
   );
 }
 
